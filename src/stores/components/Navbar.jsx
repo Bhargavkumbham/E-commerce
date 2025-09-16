bb0367187d92935fbd87147bc70b95de3ee5c039
Bhargav Kumbham <bhargavkumbham@192.168.1.9>
2025-09-16 15:34:30 +0530

    Made some changes in styling

--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
           to="/"
           className="flex items-center text-gray-900 text-2xl font-extrabold tracking-tight"
         >
-          <img src="/assets/logo.png" alt="E-mart" className="h-24 w-auto" />
+          <img src="/assets/logo.png" alt="E-mart" className="h-30 w-auto" />
         </Link>
 
         <form
